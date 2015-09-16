7473f9b6f2a32c94d8dbda55ae47c54d56851eb7
Ellis Michael <emichael@cs.washington.edu>
2015-09-16 12:27:37 -0700

    adding a bit of beginning documentation

--- pyrem/host.py
+++ pyrem/host.py
@@ -1,2 +1,9 @@
+"""Contains the class that controls a remote host.
+
+This host object should be a simple wrapper around ssh/scp with a easy, simple
+interface. Commands should be able to be executed synchronously or
+asynchronously.
+"""
+
 class Host(object):
     pass
---
+++ pyrem/task.py
@@ -0,0 +1,9 @@
+"""Contains the main unit of execution in PyREM, the task.
+
+Tasks should be composable and should be able to be executed synchronously or
+asynchronously. There should be some easy way of combining tasks both
+sequentially and in parallel.
+"""
+
+class Task(object):
+    pass
