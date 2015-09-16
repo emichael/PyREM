7035273d4c0c6e55e1b5bd2a7135c7ba0c6cc208
Ellis Michael <emichael@cs.washington.edu>
2015-09-16 14:56:58 -0700

    First pass at a basic host object

--- pyrem/host.py
+++ pyrem/host.py
@@ -5,5 +5,26 @@ interface. Commands should be able to be executed synchronously or
 asynchronously.
 """
 
+from subprocess import Popen
+
+
 class Host(object):
-    pass
+
+    def __init__(self, hostname):
+        self.__hostname = hostname
+
+    def run(self, command, wait=True):
+        """Run a command on the remote host.
+
+        Args:
+            command: Either a string or a list. The command to execute on the
+                remote host.
+
+            wait: Boolean. Whether or not to wait for the command to finish on
+                the remote host. Default True.
+        """
+        if isinstance(command, list):
+            command = ' '.join(command)
+        proc = Popen(['ssh', self.__hostname, command], shell=False)
+        if wait:
+            proc.wait()
