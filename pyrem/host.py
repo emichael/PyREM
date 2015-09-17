e00ff50bba4a1dc137c5ee6afc876341b34397a0
Ellis Michael <emichael@cs.washington.edu>
2015-09-17 15:32:11 -0700

    Added some task scaffolding

--- pyrem/host.py
+++ pyrem/host.py
@@ -5,65 +5,41 @@ interface. Commands should be able to be executed synchronously or
 asynchronously.
 """
 
-import os
-
-from subprocess import Popen
-
-
-DEVNULL = file(os.devnull, 'w')
-
+from pyrem.task import SubprocessTask
 
 class Host(object):
 
     def __init__(self, hostname):
         self.__hostname = hostname
-        self.__waiting_on = set() # set of Popen
 
-    def _exec(self, command, wait, quiet):
-        args = {}
-        if quiet:
-            args['stdin'] = DEVNULL
-            args['stdout'] = DEVNULL
-            args['stderr'] = DEVNULL
-
-        process = Popen(command, **args) # pylint: disable=W0142
-        if wait:
-            process.wait()
-        else:
-            self.__waiting_on.add(process)
-
-    def run(self, command, wait=True, quiet=False):
+    def run(self, command, quiet=False):
         """Run a command on the remote host.
-
-        Eventually, this and the other two functions should return a Task object
-        that can be interacted with just like any other Task.
 
         Args:
             command: Either a string or a list. The command to execute on the
                 remote host.
-
-            wait: Boolean. Whether or not to wait for the command to finish on
-                the remote host. Default True.
+            quiet: Boolean. Whether or not to print process output.
+        Return:
+            A SubprocessTask.
         """
         if isinstance(command, list):
             command = ' '.join(command)
-        self._exec(['ssh', self.__hostname, command], wait, quiet)
+        return SubprocessTask(['ssh', self.__hostname, command], quiet=quiet)
 
-    def send_file(self, file_name, remote_destination=None, wait=True,
-                  quiet=False):
+    def send_file(self, file_name, remote_destination=None, quiet=False):
         if not remote_destination:
             remote_destination = file_name
 
-        self._exec(['rsync', '-ut', file_name,
-                    '%s:%s' % (self.__hostname, remote_destination)],
-                   wait, quiet)
+        return SubprocessTask(
+            ['rsync', '-ut', file_name,
+             '%s:%s' % (self.__hostname, remote_destination)],
+            quiet=quiet)
 
-    def get_file(self, file_name, local_destination=None, wait=True,
-                 quiet=False):
-        self._exec(['rsync', '-ut', '%s:%s' % (self.__hostname, file_name),
-                    local_destination], wait, quiet)
+    def get_file(self, file_name, local_destination=None, quiet=False):
+        if not local_destination:
+            local_destination = file_name
 
-    def wait_all(self):
-        for process in self.__waiting_on:
-            process.wait()
-        self.__waiting_on = set()
+        return SubprocessTask(
+            ['rsync', '-ut', '%s:%s' % (self.__hostname, file_name),
+             local_destination],
+            quiet=quiet)
--- pyrem/task.py
+++ pyrem/task.py
@@ -5,5 +5,98 @@ asynchronously. There should be some easy way of combining tasks both
 sequentially and in parallel.
 """
 
+import os
+
+from enum import Enum
+from subprocess import Popen
+
+
+TaskStatus = Enum('TaskStatus', 'IDLE STARTED STOPPED CLEANED')
+
+
 class Task(object):
-    pass
+    def __init__(self):
+        self._status = TaskStatus.IDLE
+        self.return_value = None
+
+    def run(self):
+        self.start(wait=True)
+        self.stop()
+        self.cleanup()
+
+    def start(self, wait=False):
+        if self._status is not TaskStatus.IDLE:
+            raise RuntimeError("Cannot start task in state %s" % self._status)
+        self._start()
+        self._status = TaskStatus.STARTED
+
+        if wait:
+            self.wait()
+
+    def _start(self):
+        raise NotImplementedError
+
+    def wait(self):
+        if self._status is not TaskStatus.STARTED:
+            raise RuntimeError("Cannot wait on task in state %s" % self._status)
+        self._wait()
+
+    def _wait(self):
+        pass
+
+    def stop(self):
+        if self._status is not TaskStatus.STARTED:
+            raise RuntimeError("Cannot stop task in state %s" % self._status)
+        self._stop()
+        self._status = TaskStatus.STOPPED
+
+    def _stop(self):
+        pass
+
+    def cleanup(self):
+        if self._status is not TaskStatus.STOPPED:
+            raise RuntimeError("Cannot cleanup task in state %s" % self._status)
+        self._cleanup()
+        self._status = TaskStatus.CLEANED
+
+    def _cleanup(self):
+        pass
+
+    def reset(self):
+        if self._status is not TaskStatus.CLEANED:
+            raise RuntimeError("Cannot reset task in state %s" % self._status)
+        self._reset()
+        self.return_value = None
+        self._status = TaskStatus.IDLE
+
+    def _reset(self):
+        pass
+
+
+class SubprocessTask(Task):
+    DEVNULL = file(os.devnull, 'w')
+
+
+    def __init__(self, command, quiet=False):
+        super(SubprocessTask, self).__init__()
+        self._command = command
+
+        self._kwargs = {}
+        if quiet:
+            self._kwargs['stdin'] = self.DEVNULL
+            self._kwargs['stdout'] = self.DEVNULL
+            self._kwargs['stderr'] = self.DEVNULL
+
+        self._process = None
+
+    def _start(self):
+        self._process = Popen(self._command, **self._kwargs)
+
+    def _wait(self):
+        self._process.wait()
+        self.return_value = self._process.returncode
+
+    def _stop(self):
+        if self._process.returncode is None:
+            self._process.terminate()
+            self._process.kill()
