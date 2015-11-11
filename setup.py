7f2b94b4acbb91b8af5f9b9cd8e66ddef0352d9e
Ellis Michael <r.ellis.michael@gmail.com>
2015-11-10 21:15:59 -0800

    updating dependencies

--- setup.py
+++ setup.py
@@ -3,4 +3,8 @@ from setuptools import setup
 setup(name='PyREM',
       version='0.0.1',
       description="Python Remote Experiment Runner",
+      install_requires=[
+        'enum34>=1.0.4,<2',
+        'ipython>=4,<5'
+      ],
       packages=['pyrem'])
