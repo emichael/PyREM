38ef853d5094fed4babdcbfbf6ee17b498111f6b
Ellis Michael <emichael@cs.washington.edu>
2015-11-12 12:08:42 -0800

    Updating metadata for first release

--- setup.py
+++ setup.py
@@ -1,12 +1,53 @@
+import os
+
 from setuptools import setup
 
-setup(name='PyREM',
-      version='0.1.0',
-      description="Python Remote Experiment Runner",
-      url='https://github.com/emichael/PyREM',
-      download_url='https://github.com/peterldowns/mypackage/tarball/0.1.0',
-      install_requires=[
-        'enum34>=1.0.4,<2',
-        'ipython>=4,<5'
-      ],
-      packages=['pyrem'])
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def read(*path):
+    with open(os.path.join(HERE, *path), 'r') as f:
+        return f.read()
+
+################################################################################
+NAME = 'PyREM'
+DESCRIPTION = "Python Remote Experiment Runner"
+VERSION = '0.1.0'
+URL = 'https://github.com/emichael/PyREM'
+AUTHOR = "Ellis Michael"
+AUTHOR_EMAIL = 'ellis@ellismichael.com'
+PACKAGES = ['pyrem']
+KEYWORDS = ["remote management", "distributed", "experiment"]
+LONG_DESCRIPTION = read('README.rst')
+LICENSE = "MIT"
+CLASSIFIERS = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: System Administrators',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Software Development',
+    'Topic :: System :: Distributed Computing'
+]
+INSTALL_REQUIRES = [
+    'enum34>=1.0.4,<2',
+    'ipython>=4,<5'
+]
+################################################################################
+
+if __name__ == '__main__':
+    setup(name=NAME,
+          description=DESCRIPTION,
+          version=VERSION,
+          url=URL,
+          author=AUTHOR,
+          author_email=AUTHOR_EMAIL,
+          packages=PACKAGES,
+          keywords=KEYWORDS,
+          long_description=LONG_DESCRIPTION,
+          license=LICENSE,
+          classifiers=CLASSIFIERS,
+          install_requires=INSTALL_REQUIRES
+    )
